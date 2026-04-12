4cd92da8bd52962fe519080c9bee397f1d83a22b
brodr1 <baezantonio589@gmail.com>
2026-04-11 21:23:47 -0600

    autosale-backend

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+
+import { VehiclesModule } from './modules/vehicles/vehicles.module';
+
+
+@Module({
+  imports: [ VehiclesModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
