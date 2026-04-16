c9d09ff4a336d1625fa5df4c473bc7c85d2f0c0a
brodr1 <baezantonio589@gmail.com>
2026-04-15 19:14:16 -0600

    Agregar dependencias mappeptypes awagwer

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/vi');
+
+  const config = new DocumentBuilder()
+    .setTitle('API de AutoSale')
+    .setDescription('API para la gestion de vehiculos en AutoSale')
+    .setVersion('1.0')
+    .build();
+
+    const document = SwaggerModule.createDocument(app,config);
+    SwaggerModule.setup('api/docs', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
